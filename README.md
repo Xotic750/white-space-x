a7c48731658c3754860f6252eff6e087c93e1637
Xotic750 <xotic750@gmail.com>
2017-07-11 23:40:54 +0200

    Update dependencies

--- README.md
+++ README.md
@@ -1,97 +1,88 @@
-<a name="module_white-space-x"></a>
-
-## white-space-x
 <a href="https://travis-ci.org/Xotic750/white-space-x"
-title="Travis status">
-<img src="https://travis-ci.org/Xotic750/white-space-x.svg?branch=master"
-alt="Travis status" height="18">
+   title="Travis status">
+<img
+   src="https://travis-ci.org/Xotic750/white-space-x.svg?branch=master"
+   alt="Travis status" height="18"/>
 </a>
 <a href="https://david-dm.org/Xotic750/white-space-x"
-title="Dependency status">
+   title="Dependency status">
 <img src="https://david-dm.org/Xotic750/white-space-x.svg"
-alt="Dependency status" height="18"/>
+   alt="Dependency status" height="18"/>
 </a>
 <a href="https://david-dm.org/Xotic750/white-space-x#info=devDependencies"
-title="devDependency status">
+   title="devDependency status">
 <img src="https://david-dm.org/Xotic750/white-space-x/dev-status.svg"
-alt="devDependency status" height="18"/>
+   alt="devDependency status" height="18"/>
 </a>
 <a href="https://badge.fury.io/js/white-space-x" title="npm version">
 <img src="https://badge.fury.io/js/white-space-x.svg"
-alt="npm version" height="18">
+   alt="npm version" height="18"/>
 </a>
+<a name="module_white-space-x"></a>
 
+## white-space-x
 List of ECMAScript5 white space characters.
 
-**Version**: 1.2.0  
+**Version**: 2.0.0  
 **Author**: Xotic750 <Xotic750@gmail.com>  
 **License**: [MIT](&lt;https://opensource.org/licenses/MIT&gt;)  
 **Copyright**: Xotic750  
 
 * [white-space-x](#module_white-space-x)
-    * [`~whiteSpaces`](#module_white-space-x..whiteSpaces) : <code>Array.&lt;number&gt;</code>
-    * [`~ws`](#module_white-space-x..ws) : <code>string</code>
+    * [`~list`](#module_white-space-x..list) : <code>Array.&lt;Object&gt;</code>
+    * [`~string`](#module_white-space-x..string) : <code>string</code>
 
-<a name="module_white-space-x..whiteSpaces"></a>
+<a name="module_white-space-x..list"></a>
 
-### `white-space-x~whiteSpaces` : <code>Array.&lt;number&gt;</code>
-An array of the whitespace char codes.
+### `white-space-x~list` : <code>Array.&lt;Object&gt;</code>
+An array of the ES5 whitespace char codes, string, and their descriptions.
 
-**Kind**: inner property of <code>[white-space-x](#module_white-space-x)</code>  
-**Properties**
-
-| Name | Type | Description |
-| --- | --- | --- |
-| 0 | <code>number</code> | 0x0009 // Tab |
-| 1 | <code>number</code> | 0x000a // Line Feed |
-| 2 | <code>number</code> | 0x000b // Vertical Tab |
-| 3 | <code>number</code> | 0x000c // Form Feed |
-| 4 | <code>number</code> | 0x000d // Carriage Return |
-| 5 | <code>number</code> | 0x0020 // Space |
-| 6 | <code>number</code> | 0x00a0 // No-break space |
-| 7 | <code>number</code> | 0x1680 // Ogham space mark |
-| 8 | <code>number</code> | 0x180e // Mongolian vowel separator |
-| 9 | <code>number</code> | 0x2000 // En quad |
-| 10 | <code>number</code> | 0x2001 // Em quad |
-| 11 | <code>number</code> | 0x2002 // En space |
-| 12 | <code>number</code> | 0x2003 // Em space |
-| 13 | <code>number</code> | 0x2004 // Three-per-em space |
-| 14 | <code>number</code> | 0x2005 // Four-per-em space |
-| 15 | <code>number</code> | 0x2006 // Six-per-em space |
-| 16 | <code>number</code> | 0x2007 // Figure space |
-| 17 | <code>number</code> | 0x2008 // Punctuation space |
-| 18 | <code>number</code> | 0x2009 // Thin space |
-| 19 | <code>number</code> | 0x200a // Hair space |
-| 20 | <code>number</code> | 0x2028 // Line separator |
-| 21 | <code>number</code> | 0x2029 // Paragraph separator |
-| 22 | <code>number</code> | 0x202f // Narrow no-break space |
-| 23 | <code>number</code> | 0x205f // Medium mathematical space |
-| 24 | <code>number</code> | 0x3000 // Ideographic space |
-| 25 | <code>number</code> | 0xfeff // Byte Order Mark |
-
+**Kind**: inner property of [<code>white-space-x</code>](#module_white-space-x)  
 **Example**  
 ```js
-var lib = require('white-space-x');
-var count = 0x110000;
-var nws = ''; // A string of all the non-whitepaces
-do {
-  count -= 1;
-  if (lib.whiteSpaces.indexOf(count) < 0) {
-    nws = String.fromCodePoint(count) + nws;
-  }
-} while (count);
+var whiteSpace = require('white-space-x');
+whiteSpaces.list.foreach(function (item) {
+  console.log(lib.description, item.code, item.string);
+});
 ```
-<a name="module_white-space-x..ws"></a>
+<a name="module_white-space-x..string"></a>
 
-### `white-space-x~ws` : <code>string</code>
-A string of the whitespace characters.
+### `white-space-x~string` : <code>string</code>
+A string of the ES5 whitespace characters.
 
-**Kind**: inner property of <code>[white-space-x](#module_white-space-x)</code>  
-**Default**: <code>&quot;\\u0009\\u000a\\u000b\\u000c\\u000d\\u0020\\u00a0\\u1680\\u180e\\u2000\\u2001\\u2002\\u2003\\u2004\\u2005\\u2006\\u2007\\u2008\\u2009\\u200a\\u2028\\u2029\\u202f\\u205f\\u3000\\ufeff&quot;</code>  
+**Kind**: inner property of [<code>white-space-x</code>](#module_white-space-x)  
 **Example**  
 ```js
-var lib = require('white-space-x');
-var ws = '\u0009\u000a\u000b\u000c\u000d\u0020\u00a0\u1680\u180e\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200a\u2028\u2029\u202f\u205f\u3000\ufeff';
-var re1 = new RegExp('^[' + lib.ws + ']+$)');
+var whiteSpace = require('white-space-x');
+var characters = [
+  '\u0009',
+  '\u000a',
+  '\u000b',
+  '\u000c',
+  '\u000d',
+  '\u0020',
+  '\u00a0',
+  '\u1680',
+  '\u180e',
+  '\u2000',
+  '\u2001',
+  '\u2002',
+  '\u2003',
+  '\u2004',
+  '\u2005',
+  '\u2006',
+  '\u2007',
+  '\u2008',
+  '\u2009',
+  '\u200a',
+  '\u2028',
+  '\u2029',
+  '\u202f',
+  '\u205f',
+  '\u3000',
+  '\ufeff'
+];
+var ws = characters.join('');
+var re1 = new RegExp('^[' + whiteSpace.string + ']+$)');
 re1.test(ws); // true
 ```
--- index.js
+++ index.js
@@ -1,134 +1,219 @@
 /**
- * @file
- * <a href="https://travis-ci.org/Xotic750/white-space-x"
- * title="Travis status">
- * <img src="https://travis-ci.org/Xotic750/white-space-x.svg?branch=master"
- * alt="Travis status" height="18">
- * </a>
- * <a href="https://david-dm.org/Xotic750/white-space-x"
- * title="Dependency status">
- * <img src="https://david-dm.org/Xotic750/white-space-x.svg"
- * alt="Dependency status" height="18"/>
- * </a>
- * <a href="https://david-dm.org/Xotic750/white-space-x#info=devDependencies"
- * title="devDependency status">
- * <img src="https://david-dm.org/Xotic750/white-space-x/dev-status.svg"
- * alt="devDependency status" height="18"/>
- * </a>
- * <a href="https://badge.fury.io/js/white-space-x" title="npm version">
- * <img src="https://badge.fury.io/js/white-space-x.svg"
- * alt="npm version" height="18">
- * </a>
- *
- * List of ECMAScript5 white space characters.
- *
- * @version 1.2.0
+ * @file List of ECMAScript5 white space characters.
+ * @version 2.0.0
  * @author Xotic750 <Xotic750@gmail.com>
  * @copyright  Xotic750
  * @license {@link <https://opensource.org/licenses/MIT> MIT}
  * @module white-space-x
  */
 
-/* eslint strict: 1, max-len:1 */
+'use strict';
 
-/* global module */
+var forEach = require('for-each');
 
-;(function () { // eslint-disable-line no-extra-semi
+/**
+ * An array of the ES5 whitespace char codes, string, and their descriptions.
+ *
+ * @name list
+ * @type Array.<Object>
+ * @example
+ * var whiteSpace = require('white-space-x');
+ * whiteSpaces.list.foreach(function (item) {
+ *   console.log(lib.description, item.code, item.string);
+ * });
+ */
+var list = [
+  {
+    code: 0x0009,
+    description: 'Tab',
+    string: '\u0009'
+  },
+  {
+    code: 0x000a,
+    description: 'Line Feed',
+    string: '\u000a'
+  },
+  {
+    code: 0x000b,
+    description: 'Vertical Tab',
+    string: '\u000b'
+  },
+  {
+    code: 0x000c,
+    description: 'Form Feed',
+    string: '\u000c'
+  },
+  {
+    code: 0x000d,
+    description: 'Carriage Return',
+    string: '\u000d'
+  },
+  {
+    code: 0x0020,
+    description: 'Space',
+    string: '\u0020'
+  },
+  /*
+  {
+    code: 0x0085,
+    description: 'Next line - Not ES5 whitespace',
+    string: '\u0085'
+  }
+  */
+  {
+    code: 0x00a0,
+    description: 'No-break space',
+    string: '\u00a0'
+  },
+  {
+    code: 0x1680,
+    description: 'Ogham space mark',
+    string: '\u1680'
+  },
+  {
+    code: 0x180e,
+    description: 'Mongolian vowel separator',
+    string: '\u180e'
+  },
+  {
+    code: 0x2000,
+    description: 'En quad',
+    string: '\u2000'
+  },
+  {
+    code: 0x2001,
+    description: 'Em quad',
+    string: '\u2001'
+  },
+  {
+    code: 0x2002,
+    description: 'En space',
+    string: '\u2002'
+  },
+  {
+    code: 0x2003,
+    description: 'Em space',
+    string: '\u2003'
+  },
+  {
+    code: 0x2004,
+    description: 'Three-per-em space',
+    string: '\u2004'
+  },
+  {
+    code: 0x2005,
+    description: 'Four-per-em space',
+    string: '\u2005'
+  },
+  {
+    code: 0x2006,
+    description: 'Six-per-em space',
+    string: '\u2006'
+  },
+  {
+    code: 0x2007,
+    description: 'Figure space',
+    string: '\u2007'
+  },
+  {
+    code: 0x2008,
+    description: 'Punctuation space',
+    string: '\u2008'
+  },
+  {
+    code: 0x2009,
+    description: 'Thin space',
+    string: '\u2009'
+  },
+  {
+    code: 0x200a,
+    description: 'Hair space',
+    string: '\u200a'
+  },
+  /*
+  {
+    code: 0x200b,
+    description: 'Zero width space - Not ES5 whitespace',
+    string: '\u200b'
+  },
+  */
+  {
+    code: 0x2028,
+    description: 'Line separator',
+    string: '\u2028'
+  },
+  {
+    code: 0x2029,
+    description: 'Paragraph separator',
+    string: '\u2029'
+  },
+  {
+    code: 0x202f,
+    description: 'Narrow no-break space',
+    string: '\u202f'
+  },
+  {
+    code: 0x205f,
+    description: 'Medium mathematical space',
+    string: '\u205f'
+  },
+  {
+    code: 0x3000,
+    description: 'Ideographic space',
+    string: '\u3000'
+  },
+  {
+    code: 0xfeff,
+    description: 'Byte Order Mark',
+    string: '\ufeff'
+  }
+];
 
-  'use strict';
+var string = '';
+forEach(list, function reducer(item) {
+  string += item.string;
+});
 
-  var define = require('define-properties-x');
-  var forEach = require('foreach');
-
-  /**
-   * An array of the whitespace char codes.
-   *
-   * @name whiteSpaces
-   * @type Array.<number>
-   * @property {number} 0 0x0009 // Tab
-   * @property {number} 1 0x000a // Line Feed
-   * @property {number} 2 0x000b // Vertical Tab
-   * @property {number} 3 0x000c // Form Feed
-   * @property {number} 4 0x000d // Carriage Return
-   * @property {number} 5 0x0020 // Space
-   * @property {number} 6 0x00a0 // No-break space
-   * @property {number} 7 0x1680 // Ogham space mark
-   * @property {number} 8 0x180e // Mongolian vowel separator
-   * @property {number} 9 0x2000 // En quad
-   * @property {number} 10 0x2001 // Em quad
-   * @property {number} 11 0x2002 // En space
-   * @property {number} 12 0x2003 // Em space
-   * @property {number} 13 0x2004 // Three-per-em space
-   * @property {number} 14 0x2005 // Four-per-em space
-   * @property {number} 15 0x2006 // Six-per-em space
-   * @property {number} 16 0x2007 // Figure space
-   * @property {number} 17 0x2008 // Punctuation space
-   * @property {number} 18 0x2009 // Thin space
-   * @property {number} 19 0x200a // Hair space
-   * @property {number} 20 0x2028 // Line separator
-   * @property {number} 21 0x2029 // Paragraph separator
-   * @property {number} 22 0x202f // Narrow no-break space
-   * @property {number} 23 0x205f // Medium mathematical space
-   * @property {number} 24 0x3000 // Ideographic space
-   * @property {number} 25 0xfeff // Byte Order Mark
-   * @example
-   * var lib = require('white-space-x');
-   * var count = 0x110000;
-   * var nws = ''; // A string of all the non-whitepaces
-   * do {
-   *   count -= 1;
-   *   if (lib.whiteSpaces.indexOf(count) < 0) {
-   *     nws = String.fromCodePoint(count) + nws;
-   *   }
-   * } while (count);
-   */
-  define.property(module.exports, 'whiteSpaces', [
-    0x0009, // Tab
-    0x000a, // Line Feed
-    0x000b, // Vertical Tab
-    0x000c, // Form Feed
-    0x000d, // Carriage Return
-    0x0020, // Space
-    // 0x0085, // Next line - Not ES5 whitespace
-    0x00a0, // No-break space
-    0x1680, // Ogham space mark
-    0x180e, // Mongolian vowel separator
-    0x2000, // En quad
-    0x2001, // Em quad
-    0x2002, // En space
-    0x2003, // Em space
-    0x2004, // Three-per-em space
-    0x2005, // Four-per-em space
-    0x2006, // Six-per-em space
-    0x2007, // Figure space
-    0x2008, // Punctuation space
-    0x2009, // Thin space
-    0x200a, // Hair space
-    // 0x200b, // Zero width space - Not ES5 whitespace
-    0x2028, // Line separator
-    0x2029, // Paragraph separator
-    0x202f, // Narrow no-break space
-    0x205f, // Medium mathematical space
-    0x3000, // Ideographic space
-    0xfeff // Byte Order Mark
-  ]);
-
-  var wsString = '';
-  forEach(module.exports.whiteSpaces, function (item) {
-    wsString += String.fromCharCode(item);
-  });
-
-  /**
-   * A string of the whitespace characters.
-   *
-   * @name ws
-   * @type string
-   * @default \u0009\u000a\u000b\u000c\u000d\u0020\u00a0\u1680\u180e\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200a\u2028\u2029\u202f\u205f\u3000\ufeff
-   * @example
-   * var lib = require('white-space-x');
-   * var ws = '\u0009\u000a\u000b\u000c\u000d\u0020\u00a0\u1680\u180e\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200a\u2028\u2029\u202f\u205f\u3000\ufeff';
-   * var re1 = new RegExp('^[' + lib.ws + ']+$)');
-   * re1.test(ws); // true
-   */
-  define.property(module.exports, 'ws', wsString);
-}());
+/**
+ * A string of the ES5 whitespace characters.
+ *
+ * @name string
+ * @type string
+ * @example
+ * var whiteSpace = require('white-space-x');
+ * var characters = [
+ *   '\u0009',
+ *   '\u000a',
+ *   '\u000b',
+ *   '\u000c',
+ *   '\u000d',
+ *   '\u0020',
+ *   '\u00a0',
+ *   '\u1680',
+ *   '\u180e',
+ *   '\u2000',
+ *   '\u2001',
+ *   '\u2002',
+ *   '\u2003',
+ *   '\u2004',
+ *   '\u2005',
+ *   '\u2006',
+ *   '\u2007',
+ *   '\u2008',
+ *   '\u2009',
+ *   '\u200a',
+ *   '\u2028',
+ *   '\u2029',
+ *   '\u202f',
+ *   '\u205f',
+ *   '\u3000',
+ *   '\ufeff'
+ * ];
+ * var ws = characters.join('');
+ * var re1 = new RegExp('^[' + whiteSpace.string + ']+$)');
+ * re1.test(ws); // true
+ */
+module.exports = {
+  list: list,
+  string: string
+};
