be4a129d05015b59419c528d6106d915028d3bda
Graham Fairweather <xotic750@gmail.com>
2019-07-22 14:08:00 +0200

    :bookmark: v4.0.16
    Dependency updates

--- dist/white-space-x.js
+++ dist/white-space-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2015-present",
-  "date": "2019-07-21T22:12:06.988Z",
+  "date": "2019-07-22T12:07:54.127Z",
   "describe": "",
   "description": "List of ECMAScript white space characters.",
   "file": "white-space-x.js",
   "hash": "695bf73436b64dc2b70c",
   "license": "MIT",
-  "version": "4.0.15"
+  "version": "4.0.16"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
