38439d9e5e9e36cda46da3dde3d09c12a3015359
Graham Fairweather <xotic750@gmail.com>
2019-08-15 19:25:06 +0200

    Maintenance

--- dist/white-space-x.js
+++ dist/white-space-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2015-present",
-  "date": "2019-08-14T14:28:45.346Z",
+  "date": "2019-08-15T17:25:00.415Z",
   "describe": "",
   "description": "List of ECMAScript white space characters.",
   "file": "white-space-x.js",
